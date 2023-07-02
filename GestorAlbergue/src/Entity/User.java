186b43c98aca06c71aa55878c3cd73c41cacaea6
Juan Carlos <jmondalgotapia@gmail.com>
2023-07-01 21:26:28 -0500

    Creación de User (almacena rol y nombre de usuario)

--- GestorAlbergue/src/Entity/User.java
+++ GestorAlbergue/src/Entity/User.java
@@ -1,11 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 package Entity;
 
 
 public class User {
+    private static String userName;
+    private static String rolName;
 
+    public static String getUserName() {
+        return userName;
+    }
+
+    public static String getRolName() {
+        return rolName;
+    }
+
+    public static void setUserName(String userName) {
+        User.userName = userName;
+    }
+
+    public static void setRolName(String rolName) {
+        User.rolName = rolName;
+    }
+    
 }
